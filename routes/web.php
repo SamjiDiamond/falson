1d07d7cae409ff6a6ba3cf91859fc7b418a4d83f
odejinmisa <odejinmisa@newwavesecosystem.com>
2023-01-04 22:55:24 +0100

    push changes

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,62 @@ Route::get('/', function () {
     return redirect()->route('login');
 })->name('welcome');
 
+Route::get('/ringo-tv', function () {
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://www.api.ringo.ng/api/dstv/addon',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS =>'{
+	"code":"PRWE36"
+}',
+    CURLOPT_HTTPHEADER => array(
+        'email: Holarlekano@gmail.com',
+        'password: Abayomioye'
+    ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
+
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://www.api.ringo.ng/api/dstv/addon',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS =>'{
+	"code":"PRWE36"
+}',
+    CURLOPT_HTTPHEADER => array(
+        'email: Holarlekano@gmail.com',
+        'password: Abayomioye'
+    ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
+
+})->name('login');
+
+
 Route::get('/login', function () {
     return view('auth.login');
 })->name('login');

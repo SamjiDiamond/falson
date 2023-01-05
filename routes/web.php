810d32cbf98558a9e8f7df8f7edd8406e2c63f91
odejinmisa <odejinmisa@newwavesecosystem.com>
2023-01-05 08:54:23 +0100

    push changes

--- routes/web.php
+++ routes/web.php
@@ -101,6 +101,35 @@ $response = curl_exec($curl);
 curl_close($curl);
 echo $response;
 
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://www.api.ringo.ng/api/agent/p2',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS =>'{
+"serviceCode" : "V-TV",
+"type" : "STARTIMES",
+"smartCardNo" : "01831595848"
+}',
+    CURLOPT_HTTPHEADER => array(
+        'email: Holarlekano@gmail.com',
+        'password: Abayomioye',
+        'Content-Type: application/json'
+    ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
+
 })->name('login');
 
 

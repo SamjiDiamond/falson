b5e10b191d5894b69fd51ea67b3d4eed5f5f1170
odejinmisa <odejinmisa@newwavesecosystem.com>
2024-02-29 01:26:16 +0100

    Added get education endpoint

--- app/Models/AppEducationControl.php
+++ app/Models/AppEducationControl.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class AppEducationControl extends Model
+class AppEducationControl extends Model implements Auditable
 {
     use HasFactory;
 

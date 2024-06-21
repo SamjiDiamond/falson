fde8c42660c5875caf36754ff21125d7e7ac1449
odejinmisa <odejinmisa@newwavesecosystem.com>
2024-06-21 21:02:40 +0100

    fix electricity purchase for admin

--- app/Http/Controllers/Reseller/PayController.php
+++ app/Http/Controllers/Reseller/PayController.php
@@ -209,8 +209,10 @@ class PayController extends Controller
         $air = new SellElectricityController();
 
         switch (strtolower($server)) {
-            case "6":
-                return $air->server6($request, $input['coded'], $input['phone'], $ref, $net, $request, $dada, "reseller");
+            case "1":
+                return $air->server1($request, $input['coded'], $input['phone'], $ref, $net, $request, $dada, "reseller");
+            case "2":
+                return $air->server2($request, $input['coded'], $input['phone'], $ref, $net, $request, $dada, "reseller");
             default:
                 return response()->json(['success' => 0, 'message' => 'Kindly contact system admin']);
         }

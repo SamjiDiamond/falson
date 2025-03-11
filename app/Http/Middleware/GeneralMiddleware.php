106fa7c05accb823cc3c54e2860add62c1333733
odejinmisa <odejinmisa@newwavesecosystem.com>
2025-03-11 23:41:38 +0100

    Force pin to be asked when enabled for transactions

--- app/Http/Middleware/GeneralMiddleware.php
+++ app/Http/Middleware/GeneralMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class GeneralMiddleware
 {
@@ -18,6 +19,18 @@ class GeneralMiddleware
     {
         $random_sleep_time = rand(1, 10); // Generate a random number between 1 and 10
         sleep($random_sleep_time); // Pause execution for the randomly generated number of seconds
+
+        $user = Auth::user();
+
+        if ($user->pin_enabled == 1) {
+            if ($request->get('pin') == null) {
+                return response()->json(['success' => 0, 'message' => 'Pin is required to complete your transaction']);
+            }
+
+            if ($request->get('pin') != $user->pin) {
+                return response()->json(['success' => 0, 'message' => 'Kindly provide valid Pin']);
+            }
+        }
 
         return $next($request);
     }
